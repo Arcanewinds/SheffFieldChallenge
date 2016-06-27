ae674947317c28edc5a19a26a3bff472eca6ab05
Arcanewinds <Arcanewinds@users.noreply.github.com>
2016-06-27 10:01:53 +0100

    Update InspectTarget.m

--- InspectTarget.m
+++ InspectTarget.m
@@ -1,69 +1,55 @@
 close all
 clear all
 format long
-
-%Target Definition
 lat_t = 53.4325875;
 lon_t = -1.58276;
-%Fly to Target
-OctoGuided( lat_t, lon_t, 8 )
-dataCount = 0
+OctoGuided( lat_t, lon_t, 5 )
+
 planFlag = 1;
 while(planFlag)
-    %Initially, measure distance from target
     dataCount = dataCount + 1;
-    [lat_c, lon_c, alt_c, hdg_c] = OctoPosition( );
-    latDiff = (lat_t - lat_c);
-    longDiff = (lon_t - lon_c);
-    distance = sqrt(latDiff.^2 + longDiff.^2) * 111111;
-    %Save images/location
-    image = cell(1,1);
-    if(dataCount == 1)
-        OctoCam(35);
-        image{dataCount} = OctoCam();
-        savedLocation = [lat_c, lon_c, alt_c, hdg_c];
-    else
-        OctoCam(35);
-        image{dataCount} = OctoCam();
-        savedLocation = cat(1, savedLocation, [lat_c, lon_c, alt_c, hdg_c]);
-    end
-    %If the target is less than 40m away...
+[lat_c, lon_c, alt_c, hdg_c] = OctoPosition( );
+latDiff = (lat_t - lat_c);
+longDiff = (long_t - long_c);
+distance = sqrt(latDiff.^2 + longDiff.^2) * 111111;
+
+if(dataCount == 1)
+    image = OctoCam(gimbalPitch(45));
+    savedLocation = [lat_c, lon_c, alt_c, hdg_c];
+else
+    image = cat(1, image, OctoCam(gimbalPitch(j)));
+    savedLocation = cat(1, savedLocation, [lat_c, lon_c, alt_c, hdg_c]);
+end
     if(distance < 40)
         initBearing = acosd(latDiff/distance);
-        [longPerim, latPerim, bearingTarget] = perimeterSearch(lon_t, lat_t, 5, 8, initBearing);
+        radius = 10;
+        numPoints = 8;
+        [longPerim, latPerim, bearingTarget] = perimeterSearch(lon_t, lat_t, radius, numPoints, initBearing);
         planFlag = 0;
     end
 end
-radius = 10;
-numPoints = 8;
 altitude = [5, 4, 3];
 gimbalPitch = [45, 30, 15];
+dataCount = 0;
 for j = 1:4
     for i = 1:numPoints+1
-        i
         OctoGuided( latPerim(i), longPerim(i), altitude(j), bearingTarget(i) )
         arriveFlag = 0;
         while(arriveFlag == 0)
-            distanceOld = distanceNew;
             dataCount = dataCount + 1;
             [lat_c, lon_c, alt_c, hdg_c] = OctoPosition( );
             latDiff = (lat_t - lat_c);
-            longDiff = (lon_t - lon_c);
-            distanceNew = sqrt(latDiff.^2 + longDiff.^2) * 111111
-            %Logging Data from the UAV
-            OctoCam(gimbalPitch(j));
-            image = OctoCam();
-            imwrite(image, sprintf('image%04d.jpg', i));
-            savedLocation = cat(1, savedLocation, [lat_c, lon_c, alt_c, hdg_c]);
-            
-            
-            if(distanceNew < 4)
-                if(distanceNew < distanceOld)
-                    
-                end
-                arriveFlag = 1;
+            longDiff = (long_t - long_c);
+            distance = sqrt(latDiff.^2 + longDiff.^2) * 111111;
+            if(dataCount == 1)
+                image = OctoCam(gimbalPitch(j));
+                savedLocation = [lat_c, lon_c, alt_c, hdg_c];
             else
-                 OctoGuided( latPerim(i), longPerim(i), altitude(j), bearingTarget(i) )
+                image = cat(1, image, OctoCam(gimbalPitch(j)));
+                savedLocation = cat(1, savedLocation, [lat_c, lon_c, alt_c, hdg_c]);
+            end
+            if(distance < 2)
+                arriveFlag = 1;
             end
         end
     end
